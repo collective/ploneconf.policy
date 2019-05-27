82370bfc830c421472a0cb24e6d410ab02bb42ba
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-05-27 14:39:01 +0200

    add fields

--- src/ploneconf/policy/content/training.py
+++ src/ploneconf/policy/content/training.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
+from plone.app.textfield import RichText as RichTextField
+from plone.app.z3cform.widget import RichTextFieldWidget
 from plone.autoform import directives
-
-# from plone.namedfile import field as namedfile
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-from zope import schema
 from plone.dexterity.content import Item
 from plone.supermodel import model
-from zope.interface import implementer
-from ploneconf.policy.vocabularies import TRAINING_CLASS_DURATION_TYPES
-from ploneconf.policy.vocabularies import LEVEL_TYPES
-from ploneconf.policy.vocabularies import AUDIENCE_TYPES
 from ploneconf.policy import _
+from ploneconf.policy.vocabularies import AUDIENCE_TYPES
+from ploneconf.policy.vocabularies import LEVEL_TYPES
+from ploneconf.policy.vocabularies import TRAINING_CLASS_DURATION_TYPES
+from ploneconf.policy.vocabularies import TRAINING_CLASS_ROOMS
+from zope import schema
+from zope.interface import implementer
 
 
 class ITraining(model.Schema):
@@ -27,7 +25,7 @@ class ITraining(model.Schema):
     )
 
     level = schema.Choice(
-        title=_('Duration'), description=u'', required=True, source=LEVEL_TYPES
+        title=_('Level'), description=u'', required=True, source=LEVEL_TYPES
     )
     audience = schema.Set(
         title=_('Audience'),
@@ -35,6 +33,32 @@ class ITraining(model.Schema):
         required=True,
         value_type=schema.Choice(source=AUDIENCE_TYPES),
     )
+    room = schema.Choice(
+        title=_('Room'),
+        description=u'',
+        required=True,
+        source=TRAINING_CLASS_ROOMS,
+    )
+    docs_link = schema.TextLine(
+        title=_('Documentation link'),
+        description=u'Insert a link to an online documentation.',
+        required=True,
+    )
+
+    what_learn = RichTextField(
+        title=_(u'What you will learn'), description=u'', required=False
+    )
+    directives.widget('what_learn', RichTextFieldWidget)
+
+    prerequisites = RichTextField(
+        title=_(u'Prerequisites'), description=u'', required=False
+    )
+    directives.widget('prerequisites', RichTextFieldWidget)
+
+    things_to_bring = RichTextField(
+        title=_(u'Things to bring'), description=u'', required=False
+    )
+    directives.widget('things_to_bring', RichTextFieldWidget)
 
 
 @implementer(ITraining)
